667e6a5354e2a1169c59845a0428244575c77d7c
meetanubhav <anubhavgupta.gupta28@gmail.com>
2020-01-29 17:35:47 +0530

    working on ui , creating template and also adding dynamic adress and phonenumber field

---
+++ JQuery Assingment/Assignment_2/js/animate.js
@@ -0,0 +1,143 @@
+// $(document).ready(function () {
+
+//     // adding effects to box
+
+//     $('#firstNameInput').click(function(){
+//         $('.firstNameInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#lastName').click(function(){
+//         $('.lastName').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#emailInput').click(function(){
+//         $('.emailInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#passwordInput').click(function(){
+//         $('.passwordInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#confirmPasswordInput').click(function(){
+//         $('.confirmPasswordInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#panInput').click(function(){
+//         $('.panInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#aadharInput').click(function(){
+//         $('.aadharInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#phoneNumberInput').click(function(){
+//         $('.phoneNumberInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+//     $('#addressInput').click(function(){
+//         $('.addressInput').show();
+//         $(this).attr('placeholder', '');
+//         $(this).css({'border':'1px solid teal'});
+//     });
+
+//     // after leaving the input field
+
+//     $('#firstNameInput').blur(function(){
+//         if($('#firstNameInput').val()==""){
+//             $('.firstNameInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'First Name');
+            
+//         }
+//     });
+
+//     $('#lastName').blur(function(){
+//         if($('#lastName').val()==""){
+//             $('.lastName').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'Last Name');
+            
+//         }
+//     });
+
+//     $('#emailInput').blur(function(){
+//         if($('#emailInput').val()==""){
+//             $('.emailInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'Email');
+            
+//         }
+//     });
+
+//     $('#passwordInput').blur(function(){
+//         if($('#passwordInput').val()==""){
+//             $('.passwordInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'Password');
+            
+//         }
+//     });
+
+//     $('#confirmPasswordInput').blur(function(){
+//         if($('#confirmPasswordInput').val()==""){
+//             $('.confirmPasswordInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'Confirm Password');
+            
+//         }
+//     });
+
+//     $('#panInput').blur(function(){
+//         if($('#panInput').val()==""){
+//             $('.panInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'PAN Number');
+            
+//         }
+//     });
+
+//     $('#aadharInput').blur(function(){
+//         if($('#aadharInput').val()==""){
+//             $('.aadharInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'Aadhar Number');
+            
+//         }
+//     });
+
+//     $('#phoneNumberInput').blur(function(){
+//         if($('#phoneNumberInput').val()==""){
+//             $('.phoneNumberInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'Phone Number');
+            
+//         }
+//     });
+
+//     $('#addressInput').blur(function(){
+//         if($('#addressInput').val()==""){
+//             $('.addressInput').hide();
+//             $(this).css({'border':'none'});
+//             $(this).css({'border-bottom':'2px solid teal'});
+//             $(this).attr('placeholder', 'Address');
+            
+//         }
+//     });
+
+// });
--- JQuery Assingment/Assignment_2/js/common.js
+++ JQuery Assingment/Assignment_2/js/common.js
@@ -1,51 +1,48 @@
-/*
-
-  Language : JQuery , JavaScript
-  Developer : Anubhav Gupta
-  Included file in : index.html
-  Purpose : Animation and Effects
-
-*/
 $(document).ready(function () {
-    $('#firstName').click(function(){
-        $('.firstName').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
+    var phoneCounter = 0 ; 
+    var addressCounter= 0;
+
+    function readUrl(input) {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+          
+          reader.onload = function(e) {
+            $('#profileImage').show();
+            $('#profileImage').attr('src', e.target.result);
+          }
+          
+          reader.readAsDataURL(input.files[0]);
+        }
+      }
+      
+      $("#imgInp").change(function() {
+        readUrl(this);
+      });
+     // appending and removing contact field
+
+     $("#addPhoneNumber").click(function(){
+        phoneCounter+=1;
+        console.log(phoneCounter);
+        $("#phoneNumberInputField").append(' <div id="newPhoneNumber"><input type="text" class="w-80" id="newPhoneNumber'+phoneCounter+'" placeholder="Phone Number"><button type="button" class="remove-button remove" id="removePhoneNumber"> - </button></div>');
     });
-    $('#lastName').click(function(){
-        $('.lastName').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
+    $(document).on("click",".remove",function(){
+        $(this).parents("#newPhoneNumber").remove();
+        return false;
+        phoneCounter-=1;
     });
-    $('#emailInput').click(function(){
-        $('.emailInput').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
+
+    // appending and removing address field
+
+    $("#addAddress").click(function(){
+        addressCounter+=1;
+        console.log(addressCounter);
+        $("#addressInputField").append('<div id="newAddressInputField"><label for="addressInput" class="addressInput">Address</label><input type="text" class="w-80" id="addressInput'+addressCounter+'" placeholder="Address"><button type="button" class="remove-button remove" id="removeAddess"> - </button></div>');
     });
-    $('#passwordInput').click(function(){
-        $('.passwordInput').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
+    $(document).on("click",".remove",function(){
+        $(this).parents("#newAddressInputField").remove();
+        return false;
+        addressCounter-=1;
     });
-    $('#confirmPasswordInput').click(function(){
-        $('.confirmPasswordInput').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
-    });
-    $('#panInput').click(function(){
-        $('.panInput').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
-    });
-    $('#aadharInput').click(function(){
-        $('.aadharInput').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
-    });
-    $('#phoneNumber').click(function(){
-        $('.phoneNumber').show();
-        $(this).attr('placeholder', '');
-        $(this).css({'border':'1px solid teal'});
-    });
-    
+
+
 });
