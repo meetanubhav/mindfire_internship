b980939298dd55cc54bcae4b84a65e10a8611815
meetanubhav <anubhavgupta.gupta28@gmail.com>
2020-02-27 16:39:19 +0530

    added department for student and queering relational queries

---
+++ C#/StudentsManager/StudentsManager/Models/StudentDepartment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsManager.Models
+{
+    public class StudentDepartment
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public int? DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string DepartmentLocation { get; set; }
+    }
+}
