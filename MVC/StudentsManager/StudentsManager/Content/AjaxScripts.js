a135700ad9078d36d1e55c3314d9474d12774a22
meetanubhav <anubhavgupta.gupta28@gmail.com>
2020-03-04 12:27:52 +0530

    AJAX POST , moved all MVC type folders under a single directory'

---
+++ MVC/StudentsManager/StudentsManager/Content/AjaxScripts.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    var myDataTable = $('#dataTableId').DataTable();
+    $(".js-delete").on("click", function () {
+        var button = $(this);
+        $.ajax({
+            url: "/Home/Delete/" + $(this).attr("data-student-id"),
+            contentType: 'application/json',
+            method: "DELETE",
+            success: function () {
+                button.parents("tr").remove();
+            }
+        })
+    });
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
+    $("#js-add-student").on("click", function () {
+        $('#exampleModal').modal("hide");
+        var studentData = new Object();
+        studentData.firstName = $("#FirstName").val();
+        studentData.lastName = $("#LastName").val();
+        studentData.emailAddress = $("#EmailAddress").val();
+        studentData.age = $("#Age").val();
+        studentData.gender = $("#gender").val();
+        studentData.departmentId = $("#DepartmentId").val();
+        $.ajax({
+            type: "POST",
+            url: "/Home/AddStudent",
+            data: JSON.stringify(studentData),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                markup = "<tr><td>" + response.StudentId + "</td>" +
+                "<td>" + response.FirstName + "</td>" +
+                 "<td>" + response.LastName + "</td>" +
+                 "<td>" + response.EmailAddress + "</td>" +
+                   "<td>" + response.Age + "</td>" +
+                   "<td>" + response.Gender + "</td>" +
+                   "<td>" + response.DepartmentName + "</td>" +
+                   "<td>" + response.DepartmentLocation + "</td>" +
+                   '<td><a href="/Home/Edit/' + response.StudentId + '">Edit</a> | <button data-student-id=' + response.StudentId + ' class="btn btn-sm btn-danger js-delete"> Delete </button></td>' +
+                   "</tr>";
+                tableBody = $("#dataTableId tbody");
+                tableBody.append(markup);
+            },
+            failure: function (response) {
+                alert("Failed Adding");
+            }
+        });
+    })
+});
---
+++ MVC/StudentsManager/StudentsManager/Controllers/HomeController.cs
@@ -0,0 +1,168 @@
+﻿using StudentsManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentsManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        StudentDbContext db = new StudentDbContext();
+
+        public ActionResult Index()
+        {
+            //var users = db.Students.Include("Department").ToList();
+            var data = (from user in db.Students.Include("Department")
+                        select new StudentDepartment
+                        {
+                            StudentId = user.StudentId,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            EmailAddress = user.EmailAddress,
+                            Age = user.Age,
+                            Gender = user.gender,
+                            DepartmentName = (user.Department != null) ? user.Department.DepartmentName : "N/A",
+                            DepartmentLocation = (user.Department != null) ? user.Department.DepartmentLocation : "N/A"
+                        }).OrderBy(x => x.FirstName).ToList();
+            return View(data);
+        }
+        public ActionResult AddStudent()
+        {
+            ViewBag.DepartmentName = db.Department.Select(m => new { m.DepartmentName, m.DepartmentId }).ToList();
+            return View();
+        }
+
+        public StudentDepartment GetNewRow(int id)
+        {
+              var getRow = (from user in db.Students.Include("Department").Where(x=>x.StudentId==id)
+                              select new StudentDepartment
+                              {
+                                  StudentId = user.StudentId,
+                                  FirstName = user.FirstName,
+                                  LastName = user.LastName,
+                                  EmailAddress = user.EmailAddress,
+                                  Age = user.Age,
+                                  Gender = user.gender,
+                                  DepartmentId = (user.Department != null) ? user.Department.DepartmentId : 0,
+                                  DepartmentName = (user.Department != null) ? user.Department.DepartmentName : "N/A",
+                                  DepartmentLocation = (user.Department != null) ? user.Department.DepartmentLocation : "N/A"
+                              }).FirstOrDefault();
+            return(getRow);
+        }
+        [HttpPost]
+        public ActionResult AddStudent(Student studentForm)
+        {
+                if (ModelState.IsValid)
+            {
+                //Logic to send data
+                db.Students.Add(studentForm);
+                db.SaveChanges();
+                return Json(GetNewRow(studentForm.StudentId));
+            }
+            else
+            {
+                return Content("Failure");
+            }
+        }
+        public ActionResult AddDepartment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddDepartment(Department departmentForm)
+        {
+            if (ModelState.IsValid)
+            {
+                //Logic to send data
+                db.Department.Add(departmentForm);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
+                return RedirectToAction("ViewDepartment");
+            }
+            else
+            {
+                return View(departmentForm);
+            }
+        }
+        public ActionResult ViewDepartment()
+        {
+            var user = db.Department.ToList();
+            return View(user);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                var getRow = (from user in db.Students.Include("Department").ToList()
+                              select new StudentDepartment
+                              {
+                                  StudentId = user.StudentId,
+                                  FirstName = user.FirstName,
+                                  LastName = user.LastName,
+                                  EmailAddress = user.EmailAddress,
+                                  Age = user.Age,
+                                  Gender = user.gender,
+                                  DepartmentId = (user.Department != null) ? user.Department.DepartmentId : 0,
+                                  DepartmentName = (user.Department != null) ? user.Department.DepartmentName : "N/A",
+                                  DepartmentLocation = (user.Department != null) ? user.Department.DepartmentLocation : "N/A"
+                              }).Single(m => m.StudentId == id);
+                ViewBag.DepartmentName = db.Department.Select(m => new { m.DepartmentName, m.DepartmentId }).ToList();
+                //ViewBag.DepartmentName = db.Department;
+                return View(getRow);
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, StudentDepartment studentUpdate)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                var studentFromDB = db.Students.FirstOrDefault(x => x.StudentId == id);
+                studentFromDB.FirstName = studentUpdate.FirstName;
+                studentFromDB.LastName = studentUpdate.LastName;
+                studentFromDB.EmailAddress = studentUpdate.EmailAddress;
+                studentFromDB.gender = studentUpdate.Gender;
+                studentFromDB.Age = studentUpdate.Age;
+                studentFromDB.DepartmentId = studentUpdate.DepartmentId;
+                db.Entry(studentFromDB).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpDelete]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound("404 not found. Better luck Next time");
+            }
+            try
+            {
+                db.Students.Remove(db.Students.Single(m => m.StudentId == id));
+                db.SaveChanges();
+                return Content("Success");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
